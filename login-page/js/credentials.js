030f915b4578f3cbe8ed3519f1d98c74eb821e2f
Alvin Joby <alvinjobyindia@gmail.com>
2025-03-28 21:34:19 +0530

    Update credentials.js

--- login-page/js/credentials.js
+++ login-page/js/credentials.js
@@ -1,5 +1,8 @@
 export const users = [
     { name: "Abhinav", email: "abhinav@gmail.com", password: "abhinav123" },
     { name: "Akshay", email: "akshay@gmail.com", password: "akshay123" },
-    { name: "hlo", email: "hlo@hlo.com", password: "222" }
+    { name: "Benson", email: "benson@gmail.com", password: "benson123" },
+    { name: "Sebin", email: "sebin@gmail.com", password: "sebin123" },
+    { name: "Mohith", email: "mohith@gmail.com", password: "mohith123" },
+    { name: "Alvin", email: "alvin@gmail.com", password: "alvin123" }
 ];
