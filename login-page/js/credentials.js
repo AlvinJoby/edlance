1444fffb6c79caeab75f0398b00410ab022b6707
Alvin Joby <alvinjobyindia@gmail.com>
2025-03-28 21:01:11 +0530

    Add files via upload

---
+++ login-page/js/credentials.js
@@ -0,0 +1,5 @@
+export const users = [
+    { name: "Abhinav", email: "abhinav@gmail.com", password: "abhinav123" },
+    { name: "Akshay", email: "akshay@gmail.com", password: "akshay123" },
+    { name: "hlo", email: "hlo@hlo.com", password: "222" }
+];
---
+++ login-page/js/main1.js
@@ -0,0 +1,14 @@
+import { users } from "./credentials.js";
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const user = users.find(user => user.email === email && user.password === password);
+
+    if (user) {
+        window.location.href = `/home-page/home1.html?name=${encodeURIComponent(user.name)}&email=${encodeURIComponent(user.email)}`;
+    }
+});
